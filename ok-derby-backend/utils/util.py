21b0af06dfc0a0c61e4382496d0d136690389547
Akiba <36563862+Akegarasu@users.noreply.github.com>
2021-08-28 21:25:25 +0800

    Create util.py

---
+++ ok-derby-backend/utils/util.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import win32gui
+
+def is_game_running() -> bool:
+    h_wnd = win32gui.FindWindow("UnityWndClass", "umamusume")
+    if not h_wnd:
+        return False
+    else:
+        return True
+
+def app_path() -> str:
+    """Returns the base application path."""
+    if hasattr(sys, 'frozen'):
+        # Handles PyInstaller
+        return os.path.dirname(sys.executable)
+    return os.path.join(os.path.dirname(__file__), "..")
