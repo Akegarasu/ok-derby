9a6a6712562ae8df262ab04a5bb0ab93ae0d9ab5
Akiba <36563862+Akegarasu@users.noreply.github.com>
2021-08-28 21:54:55 +0800

    Update

---
+++ ok-derby-backend/pack.cmd
@@ -0,0 +1,11 @@
+rmdir /s/q dist
+D:\Python\python38\python.exe -m PyInstaller main.spec
+copy .\ocr_labels.csv .\dist\main\
+copy .\single_mode_choices.csv .\dist\main\
+copy .\single_mode_races.jsonl .\dist\main\
+copy .\pyrightconfig.csv .\dist\main\
+copy .\version .\dist\main\
+xcopy .\auto_derby\templates .\dist\main\auto_derby\templates\ /s /f /h
+xcopy ..\..\ok-derby-frontend\dist .\dist\main\frontend\dist\ /s /f /h
+md .\dist\main\log
+.\dist\main\main.exe
