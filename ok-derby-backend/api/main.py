2aa4426cfc39555f2c88b6e0cc8d9a82d5ab4b47
Akiba <36563862+Akegarasu@users.noreply.github.com>
2021-08-28 21:58:57 +0800

    Create main.py

---
+++ ok-derby-backend/api/main.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import tornado.web
+
+import api.base
+# import config
+# import update
+
+
+# noinspection PyAbstractClass
+class MainHandler(tornado.web.StaticFileHandler):
+    """为了使用Vue Router的history模式，把不存在的文件请求转发到index.html"""
+    async def get(self, path, include_body=True):
+        try:
+            await super().get(path, include_body)
+        except tornado.web.HTTPError as e:
+            if e.status_code != 404:
+                raise
+            # 不存在的文件请求转发到index.html，交给前端路由
+            await super().get('index.html', include_body)
+
+
+# noinspection PyAbstractClass
+class ServerInfoHandler(api.base.ApiHandler):
+    async def get(self):
+        pass
+        # cfg = config.get_config()
+        # self.write({
+        #     'version': update.VERSION,
+        #     'config': {
+        #         'enableTranslate': cfg.enable_translate,
+        #         'loaderUrl': cfg.loader_url
+        #     }
+        # })
