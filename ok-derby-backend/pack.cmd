0b130177a7efd31193485bac3c874f7c847e2ab2
Akiba <36563862+Akegarasu@users.noreply.github.com>
2021-08-29 22:33:04 +0800

    typo

--- ok-derby-backend/pack.cmd
+++ ok-derby-backend/pack.cmd
@@ -1,4 +1,4 @@
-rmdir /s/q dist
+rmdir /s/q .\dist\main
 D:\Python\python38\python.exe -m PyInstaller main.spec
 copy .\ocr_labels.csv .\dist\main\
 copy .\single_mode_choices.csv .\dist\main\
@@ -6,6 +6,6 @@ copy .\single_mode_races.jsonl .\dist\main\
 copy .\pyrightconfig.csv .\dist\main\
 copy .\version .\dist\main\
 xcopy .\auto_derby\templates .\dist\main\auto_derby\templates\ /s /f /h
-xcopy ..\..\ok-derby-frontend\dist .\dist\main\frontend\dist\ /s /f /h
+xcopy ..\ok-derby-frontend\dist .\dist\main\frontend\dist\ /s /f /h
 md .\dist\main\log
 .\dist\main\main.exe
