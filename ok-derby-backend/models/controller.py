89523a6e41ebba3f08d36814484638772f3805a4
Akiba <36563862+Akegarasu@users.noreply.github.com>
2021-08-29 11:26:37 +0800

    chore(controller): typings

--- ok-derby-backend/models/controller.py
+++ ok-derby-backend/models/controller.py
@@ -1,24 +1,25 @@
 import time
 import logging
 import threading
-from typing import Optional, Callable
+from typing import Optional, Callable, Union, Text
 
 logger = logging.getLogger(__name__)
 
 
 class missionController:
-    FLAG = False
-    okderby = None
+    FLAG: bool = False
+    okderby: Union[Callable, None] = None
+    job_name: Text = ""
     watch_thread = threading.Thread()
     auto_derby_thread = threading.Thread()
 
     @classmethod
-    def init(cls, func: Callable):
+    def init(cls, func: Callable) -> None:
         cls.okderby = func
         cls.watch_thread = threading.Thread(target=cls.watch).start()
 
     @classmethod
-    def watch(cls):
+    def watch(cls) -> None:
         while True:
             logger.debug(f"auto_derby_thread: {cls.auto_derby_thread.is_alive()} FLAG: {cls.FLAG}")
             if not cls.auto_derby_thread.is_alive():
@@ -31,15 +32,17 @@ class missionController:
                       plugins: str,
                       adb_address: Optional[str] = None) -> None:
         cls.FLAG = True
+        cls.job_name = job_name
         cls.auto_derby_thread = threading.Thread(target=cls.okderby, args=(job_name, plugins, adb_address))
         cls.auto_derby_thread.start()
 
     @classmethod
     def stop_mission(cls) -> None:
         cls.FLAG = False
+        cls.job_name = ""
 
     @classmethod
-    def get_mission_status(cls):
+    def get_mission_status(cls) -> Text:
         if cls.auto_derby_thread.is_alive():
             return "running"
         else:
