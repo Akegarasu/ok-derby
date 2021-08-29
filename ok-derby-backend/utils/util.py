428f89ed311a91e80e868dfb2a65b79fa249965a
Akiba <36563862+Akegarasu@users.noreply.github.com>
2021-08-29 21:46:34 +0800

    typo

--- ok-derby-backend/utils/util.py
+++ ok-derby-backend/utils/util.py
@@ -2,12 +2,14 @@ import os
 import sys
 import win32gui
 
+
 def is_game_running() -> bool:
     h_wnd = win32gui.FindWindow("UnityWndClass", "umamusume")
     if not h_wnd:
         return False
     else:
         return True
+
 
 def app_path() -> str:
     """Returns the base application path."""
