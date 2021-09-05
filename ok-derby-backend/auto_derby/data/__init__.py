b9681c20dde5348e1f2b3e60dd07ba62273b1f33
Akiba <36563862+Akegarasu@users.noreply.github.com>
2021-09-05 15:34:12 +0800

    fix(data): executable path

--- ok-derby-backend/auto_derby/data/__init__.py
+++ ok-derby-backend/auto_derby/data/__init__.py
@@ -6,8 +6,9 @@ from __future__ import annotations
 
 from typing import Text
 import os
+import sys
 
-__dirname__ = os.path.dirname(os.path.abspath(__file__))
+__dirname__ = os.path.dirname(os.path.abspath(__file__)) if hasattr(sys, 'frozen') else os.path.dirname(sys.executable)
 
 
 def path(*paths: Text) -> Text:
