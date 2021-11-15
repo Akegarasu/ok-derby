f115c5eda4e144cdc1ac7ee3f59c19e838ed7fd7
Akiba <36563862+Akegarasu@users.noreply.github.com>
2021-11-15 09:27:23 +0800

    :bug: 修复无法使用adb的问题

--- ok-derby-backend/api/mission.py
+++ ok-derby-backend/api/mission.py
@@ -4,6 +4,7 @@ import api.base
 from utils.util import is_game_running
 
 from models.resp import response
+from models.settings import get_settings
 from models.controller import missionController
 
 logger = logging.getLogger(__name__)
@@ -18,7 +19,7 @@ class StartHandler(api.base.ApiHandler):
             return
         job_name = self.json_args.get("job_name")
         plugins = self.json_args.get("plugins")
-        adb_address = self.json_args.get("adb_address", None)
+        adb_address = get_settings().get("ADB_ADDRESS", None)
         if not is_game_running() and not adb_address:
             self.write(
                 response.error('游戏未运行，请启动游戏再运行')
