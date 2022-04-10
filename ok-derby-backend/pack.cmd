956417d1abc906f2aed3922dc2d97bc360a84add
Akiba <akegarasu.akiba@gmail.com>
2022-04-10 20:32:33 +0800

    update: pack.cmd

--- ok-derby-backend/pack.cmd
+++ ok-derby-backend/pack.cmd
@@ -1,13 +1,7 @@
 rmdir /s/q .\dist\main
 D:\Python\python38\python.exe -m PyInstaller main.spec
-copy .\ocr_labels.csv .\dist\main\
-copy .\single_mode_choices.csv .\dist\main\
-copy .\single_mode_races.jsonl .\dist\main\
-copy .\pyrightconfig.csv .\dist\main\
-copy .\version .\dist\main\
 xcopy .\auto_derby\templates .\dist\main\auto_derby\templates\ /s /f /h
 xcopy .\auto_derby\data .\dist\main\auto_derby\data\ /s /f /h
 xcopy .\data .\dist\main\data\ /s /f /h
 xcopy ..\ok-derby-frontend\dist .\dist\main\frontend\dist\ /s /f /h
 md .\dist\main\log
-REM .\dist\main\main.exe
